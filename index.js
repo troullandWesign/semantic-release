5dacf6cdda4b4991956f2ca80f52f26296c30e74
Thomas ROULLAND <troulland@wesign.fr>
2024-11-29 10:15:19 +0100

    feat: multiplication

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ app.get('/division', (req, res) => {
     res.json({ result: division });
 });
 
+//ajout de la multiplication
+app.get('/multiplication', (req, res) => {
+    const { a, b } = req.query;
+    const multiplication = parseFloat(a) * parseFloat(b);
+    console.log('multiplication', multiplication);
+    res.json({ result: multiplication });
+});
+
 app.listen(port, () => {
     console.log(`Serravssser is running at http://localhost:${port}`);
 });
